187264506227f764a1ea0c91677a7ee2edd57f83
Rohit Kumar <67968330+iCriptSnappie@users.noreply.github.com>
2024-06-21 15:09:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "7599658"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "4ef7f662cfc91d9920a2d7349fd5d3db"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6736620260"
+SUPPORT_GRP = "BESTIESZONE2"
+UPDATE_CHNL = "TheBytePedia"
+OWNER_USERNAME = "ll_Feelings_ll"
 
